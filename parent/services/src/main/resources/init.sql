1cd4849dd21a0b17f8dbbf2d6494a70c7f412a16
Ilkka Seppala <iluwatar@gmail.com>
2013-12-21 22:23:51 +0200

    init sql scripts are run on startup

---
+++ parent/services/src/main/resources/init.sql
@@ -0,0 +1,2 @@
+delete from user;
+insert into user values (1, "abc123", "fartti");
--- parent/web/src/main/java/com/iluwatar/tasklist/TasklistApplication.java
+++ parent/web/src/main/java/com/iluwatar/tasklist/TasklistApplication.java
@@ -2,7 +2,12 @@ package com.iluwatar.tasklist;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.iluwatar.tasklist.services.service.UserService;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -11,6 +16,12 @@ import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
  */
 public class TasklistApplication extends WebApplication
 {    	
+	
+	final static Logger logger = LoggerFactory.getLogger(TasklistApplication.class);
+	
+	@SpringBean
+	UserService userService;
+	
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
@@ -32,6 +43,8 @@ public class TasklistApplication extends WebApplication
 		getComponentInstantiationListeners().add(spring);
 		getBehaviorInstantiationListeners().add(spring);
 		spring.inject(this);
+		
+		logger.info("user count={}", userService.findAll().size());
 	}
 	
 	protected SpringComponentInjector newInjector() {
