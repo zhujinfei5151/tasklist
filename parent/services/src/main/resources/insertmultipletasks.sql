e22e35494d4038730817b8b8ba2b40695607206a
Ilkka Seppala <iluwatar@gmail.com>
2014-01-05 14:49:37 +0200

    database is initialized with multiple tasks

---
+++ parent/services/src/main/resources/insertmultipletasks.sql
@@ -0,0 +1,5 @@
+insert into `task` (id, donedate, description, done, ordernum, tasklist_id) values (1, null, "vie mummo kauppaan", 0, 1, 1);
+insert into `task` (id, donedate, description, done, ordernum, tasklist_id) values (2, null, "tee läksyt", 0, 2, 1);
+insert into `task` (id, donedate, description, done, ordernum, tasklist_id) values (3, null, "uusi kirjastokortti", 0, 3, 1);
+insert into `task` (id, donedate, description, done, ordernum, tasklist_id) values (4, null, "aloita viiksien kasvatus", 0, 4, 1);
+insert into `task` (id, donedate, description, done, ordernum, tasklist_id) values (5, null, "osta uusi auto", 0, 5, 1);
