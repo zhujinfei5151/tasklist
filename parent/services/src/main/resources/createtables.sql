bb3731773be47238911ef0c07786877e39265e5c
Ilkka Seppala <iluwatar@gmail.com>
2014-01-21 23:02:34 +0200

    update createtables.sql script

--- parent/services/src/main/resources/createtables.sql
+++ parent/services/src/main/resources/createtables.sql
@@ -4,11 +4,13 @@ USE `tasklist`;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   `passwordHash` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `tasklist` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -17,19 +19,19 @@ CREATE TABLE IF NOT EXISTS `tasklist` (
   PRIMARY KEY (`id`),
   KEY `FK_hodpdl33lpsx5jjf38j4rr3bi` (`user_id`),
   CONSTRAINT `FK_hodpdl33lpsx5jjf38j4rr3bi` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` datetime DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `done` bit(1) NOT NULL,
-  `orderNum` int(11) NOT NULL,
+  `donedate` datetime DEFAULT NULL,
+  `ordernum` int(11) NOT NULL,
   `tasklist_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_byr4jh14ok1cu0m2usfg5b5pu` (`tasklist_id`),
   CONSTRAINT `FK_byr4jh14ok1cu0m2usfg5b5pu` FOREIGN KEY (`tasklist_id`) REFERENCES `tasklist` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 delete from `task`;
 delete from `tasklist`;
