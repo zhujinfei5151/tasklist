f224d5f28bce9d3052b9eda52439819a94ad0b20
Ilkka Seppala <iluwatar@gmail.com>
2014-01-24 23:18:21 +0200

    page title updated

--- parent/web/src/main/java/com/iluwatar/tasklist/web/TasklistConstants.java
+++ parent/web/src/main/java/com/iluwatar/tasklist/web/TasklistConstants.java
@@ -2,7 +2,6 @@ package com.iluwatar.tasklist.web;
 
 public class TasklistConstants {
 
-	public static String APP_NAME = "Freetasklist.com";
 	public static String PAGE_PARAM_TASKLIST_ID = "TASKLIST_ID";
 	public static int PASSWORD_MINIMUM_LENGTH = 6;
 	
--- parent/web/src/main/java/com/iluwatar/tasklist/web/page/BasePage.java
+++ parent/web/src/main/java/com/iluwatar/tasklist/web/page/BasePage.java
@@ -9,6 +9,7 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.iluwatar.tasklist.web.TasklistConstants;
@@ -47,7 +48,7 @@ public class BasePage extends WebPage {
 		Navbar navbar = new Navbar("navbar");
 		add(navbar);
 		navbar.setPosition(Position.TOP);
-		navbar.brandName(Model.of(TasklistConstants.APP_NAME));
+		navbar.brandName(new ResourceModel("app.name"));
 		navbar.fluid();
 
 		NavbarButton homeButton = new NavbarButton(DashboardPage.class, Model.of("Dashboard")) {
