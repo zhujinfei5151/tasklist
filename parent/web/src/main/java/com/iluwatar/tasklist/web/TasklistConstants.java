4af688f123c93d4867ce130c049314a44cac4551
Ilkka Seppala <iluwatar@gmail.com>
2014-02-01 15:33:11 +0200

    changed to prettier query string

--- parent/web/src/main/java/com/iluwatar/tasklist/web/TasklistConstants.java
+++ parent/web/src/main/java/com/iluwatar/tasklist/web/TasklistConstants.java
@@ -2,7 +2,7 @@ package com.iluwatar.tasklist.web;
 
 public class TasklistConstants {
 
-	public static String PAGE_PARAM_TASKLIST_ID = "TASKLIST_ID";
+	public static String PAGE_PARAM_TASKLIST_ID = "tasklist";
 	public static int PASSWORD_MINIMUM_LENGTH = 6;
 	
 }
